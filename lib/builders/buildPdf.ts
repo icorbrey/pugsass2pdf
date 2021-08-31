20c97cc479c406c30652aa46531df2174f0be267
Isaac <icorbrey@gmail.com>
2021-08-30 21:10:15 -0400

    feat: Add PDF builder

--- lib/builders/buildPdf.ts
+++ lib/builders/buildPdf.ts
@@ -1,4 +1,18 @@
+import wkhtmltopdf from 'wkhtmltopdf'
+import fs from 'fs'
+
 export const buildPdf = () =>
 {
-	console.log('PDF build function not yet created.')
+	const htmlPath = 'build/index.html'
+	const pdfPath = 'build/build.pdf'
+
+	const html = fs.readFileSync(htmlPath).toString()
+	wkhtmltopdf(html, {
+		output: pdfPath,
+		marginTop: '0in',
+		marginLeft: '0in',
+		marginRight: '0in',
+		marginBottom: '0in',
+	})
+	console.log(`PDF file saved to '${pdfPath}'.`)
 }
